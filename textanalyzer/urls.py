9a59a5f49fda774716aef0985d43d105ba90367c
Evans-23-Oyooh <73794240+Evans-23-Oyooh@users.noreply.github.com>
2026-01-16 00:35:03 +0300

    Remove allauth.urls from urlpatterns

--- textanalyzer/urls.py
+++ textanalyzer/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
+
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='auth/password_reset.html'), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='auth/password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='auth/password_reset_confirm.html'), name='password_reset_confirm'),
