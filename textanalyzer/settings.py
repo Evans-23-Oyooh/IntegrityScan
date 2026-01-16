c2fd5c8efc86cebc17448db56851581106dc5cdc
Evans-23-Oyooh <73794240+Evans-23-Oyooh@users.noreply.github.com>
2026-01-16 09:51:12 +0300

    Remove problematic middleware - use manage.py migrate instead

--- textanalyzer/settings.py
+++ textanalyzer/settings.py
@@ -21,7 +21,6 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'analyzer.middleware.InitializeDBMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
